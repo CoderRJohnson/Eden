0f36390efd3edb6a2911bbeba678c8e94d3d6676
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-06-09 11:05:10 +0800

    Update globals.hpp

--- contracts/eden/include/globals.hpp
+++ contracts/eden/include/globals.hpp
@@ -59,12 +59,7 @@ namespace eden
       global_singleton global_stats;
 
      public:
-      globals(eosio::name contract)
-          : global_stats(contract, default_scope)
-      {
-      }
-      
-      explicit globals(eosio::name contract);
+      explicit globals(eosio::name contract): global_stats(contract, default_scope);
       explicit globals(eosio::name contract, const global_data_v1& initial_value);
       const global_data_v1& get() { return data; }
       void check_active() const;
