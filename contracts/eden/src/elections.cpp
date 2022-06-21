e58ac71d47173b7827e129c4d3d00bb060fee2d3
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-06-21 21:54:44 +0800

    Update elections.cpp

--- contracts/eden/src/elections.cpp
+++ contracts/eden/src/elections.cpp
@@ -533,7 +533,7 @@ namespace eden
       {
          if (iter->status() == member_status::active_member)
          {
-            eosio::check(iter->nft_template_id() == 453163 || iter->nft_template_id() == 453165 || iter->nft_template_id() == 453164, "error10" + iter->account().to_string());
+            eosio::check(iter->nft_template_id() == 453163 || iter->nft_template_id() == 453165, "error10" + iter->account().to_string());
          }
          ++iter;
       }
