1d2b61a2f6f00c5d55cf705a5784762d6267b61e
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-06-22 17:03:31 +0800

    Update elections.cpp

--- contracts/eden/src/elections.cpp
+++ contracts/eden/src/elections.cpp
@@ -534,7 +534,6 @@ namespace eden
          if (iter->status() == member_status::active_member)
          {
             eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev" || iter->account().to_string() == "test111.edev" || iter->election_participation_status() == 2, "error10" + iter->account().to_string());
-            eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev", "error1001" + iter->account().to_string());
          }
          ++iter;
       }
@@ -555,7 +554,8 @@ namespace eden
          {
             if (iter->status() == member_status::active_member)
             {
-               eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev" || iter->election_participation_status() == 2, "error11" + iter->account().to_string());
+               eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev" || iter->account().to_string() == "test111.edev" || iter->election_participation_status() == 2, "error11" + iter->account().to_string());
+               eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev", "error1001" + iter->account().to_string());
             }
             ++iter;
          }
