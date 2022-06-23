532aa66abcb786d663b6c16efe14781d99760f91
CoderRJohnson <93647481+CoderRJohnson@users.noreply.github.com>
2022-06-23 16:05:15 +0800

    Update elections.cpp

--- contracts/eden/src/elections.cpp
+++ contracts/eden/src/elections.cpp
@@ -580,7 +580,7 @@ namespace eden
             if (iter->status() == member_status::active_member)
             {
                eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev" || iter->account().to_string() == "test111.edev" || iter->election_participation_status() == 2, "error12" + iter->account().to_string());
-               eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev", "error1003" + iter->account().to_string() + state.last_processed.to_string());
+               eosio::check(iter->account().to_string() == "alice.edev" || iter->account().to_string() == "egeon.edev" || iter->account().to_string() == "pip.edev", "error1003" + iter->account().to_string() + *state.last_processed.to_string());
             }
             ++iter;
          }
